89eb85e29e81e419fed22f4c491b8dc3a1406f1e
andrewtso37 <atso@sfu.ca>
2015-02-24 22:59:29 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// calculation functions
+function s(v) { document.getElementById('res').value = v }
+function a(v) { document.getElementById('res').value += v }
+function e() { try { s(eval(document.getElementById('res').value)) } catch(e) { s('Error') } }
