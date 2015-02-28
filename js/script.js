9d53fae67d7a74074acc8f64b7f1970f919ecd52
andrewtso37 <atso@sfu.ca>
2015-02-28 14:29:34 -0800

    Added backspace button and onClick event to clear screen

--- js/script.js
+++ js/script.js
@@ -3,6 +3,15 @@ function s(v) { document.getElementById('res').value = v }
 function a(v) { document.getElementById('res').value += v }
 function e() { try { s(eval(document.getElementById('res').value)) } catch(e) { s('Error') } }
 
+// backspace button
+function backspace() {
+  var input, num;
+  input = document.getElementById('res');
+  num = input.value;
+  input.value = num.slice(0,num.length-1);
+  return false;
+}
+
 // hammer js
 var myElement = document.getElementById('myElement');
 
