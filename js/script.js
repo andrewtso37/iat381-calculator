5a71d5e7f3b243dc9a50636604d2793c32ae027e
WhyNotOnions <wcl6@sfu.ca>
2015-03-02 22:41:06 -0800

    additional clear gesture with display interaction

--- js/script.js
+++ js/script.js
@@ -55,18 +55,18 @@ mc.on('swiperight', function(e) {
 });
 
 // hammer js pinch
-var clearElement = document.getElementById('clearElement');
+var inputDisplay = document.getElementById('res');
 
-var mc2 = new Hammer(clearElement);
+var mc2 = new Hammer(inputDisplay);
 
 mc2.get('pinch').set({ enable: true });
+mc2.get('swipe').set({direction: Hammer.DIRECTION_ALL, velocity:0.1, threshold:1});
 
 // listen for events...
-mc2.on('pinchout', function(e) {
-
-  $('#res').addClass('backAnimation');
-  var displayEqual = document.getElementById('res');
-  document.getElementById('res').style.webkitAnimation = 'none';
-  setTimeout(function() {displayEqual.style.webkitAnimation = '';}, 0);
-  clearElement.textContent = s('').value;
+mc2.on('swipe pinch', function(e) {
+	$('#res').addClass('backAnimation');
+	var displayEqual = document.getElementById('res');
+	document.getElementById('res').style.webkitAnimation = 'none';
+	setTimeout(function() {displayEqual.style.webkitAnimation = '';}, 0);
+	clearAll();
 });
